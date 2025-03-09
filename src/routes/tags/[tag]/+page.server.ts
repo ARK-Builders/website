9fb68d8d2c53c19715d06dfa6e82834038961e3f
Kawsar Ahmed <prokawsar@gmail.com>
2025-03-09 20:34:19 +0600

    Update UI for selected tag (#24)
    * Selected tag UI update
    * Selected tag bg color
    * Progress loading bar
    * Fix selected tag color
    * Fix tag case

--- src/routes/tags/[tag]/+page.server.ts
+++ src/routes/tags/[tag]/+page.server.ts
@@ -1,14 +1,14 @@
 export const prerender = true
 
-import type { Blog, Tag } from '$utils/constants'
+import type { Blog, TagType } from '$utils/constants'
 import { getEntries, getTags } from '$utils/entries'
 import { error } from '@sveltejs/kit'
 import { slug } from 'github-slugger'
 
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
-	const tags = (getTags() as Tag[]) || []
-	return tags.map((tag: Tag) => ({ tag: tag.slug }))
+	const tags = (getTags() as TagType[]) || []
+	return tags.map((tag: TagType) => ({ tag: tag.slug }))
 }
 
 function slugsArray(tags: string[]) {
@@ -19,7 +19,7 @@ function slugsArray(tags: string[]) {
 export async function load({ params }) {
 	const { tag } = params
 	const posts = (getEntries('posts') as Blog[]) || []
-	const filteredPosts = posts.filter((post) => slugsArray(post.tags).includes(tag))
+	const filteredPosts = posts.filter((post) => slugsArray(post.tags).includes(slug(tag)))
 
 	if (!filteredPosts) {
 		throw error(404, 'No post found')
