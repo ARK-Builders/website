32c2f62286e15bbe56ecda48b9c30fd351ffa4e4
Kawsar Ahmed <prokawsar@gmail.com>
2025-03-18 15:43:00 +0600

    margin

--- src/routes/apps/[slug]/privacy-policy/+page.svelte
+++ src/routes/apps/[slug]/privacy-policy/+page.svelte
@@ -10,7 +10,7 @@
 <Head title="{app.title} | Privacy policy" />
 
 <div class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-4xl xl:px-0">
-	<div class="prose w-full max-w-none text-justify">
+	<div class="prose my-3 w-full max-w-none text-justify">
 		<RenderMarkdown content={app.content} />
 	</div>
 </div>
